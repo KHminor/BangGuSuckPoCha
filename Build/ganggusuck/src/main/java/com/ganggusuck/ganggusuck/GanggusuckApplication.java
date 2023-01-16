33e5e12be8ce705f5b754e861f99241506d33214
KHminor <ghdals2020@gmail.com>
2023-01-17 01:41:12 +0900

    Feat : 추가 build data

---
+++ Build/ganggusuck/src/main/java/com/ganggusuck/ganggusuck/GanggusuckApplication.java
@@ -0,0 +1,13 @@
+package com.ganggusuck.ganggusuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanggusuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanggusuckApplication.class, args);
+	}
+
+}
---
+++ Build/ganggusuck/src/test/java/com/ganggusuck/ganggusuck/GanggusuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganggusuck.ganggusuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanggusuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
