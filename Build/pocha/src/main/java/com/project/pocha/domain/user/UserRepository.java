b9f14bb91c85aac8465bbc4f6f3825d6da14fdaa
susu4788 <susu4788@naver.com>
2023-01-18 08:57:01 +0900

    !BREAKING CHANGE : 추가 aws rdb

---
+++ Build/pocha/src/main/java/com/project/pocha/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.project.pocha.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
