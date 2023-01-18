85d5fa148c4068afd0d02e968abc4520744fa90e
susu4788 <susu4788@naver.com>
2023-01-18 10:55:45 +0900

    Feat : 추가 CrossOrigin

--- Build/pocha/src/main/java/com/project/pocha/controller/MainController.java
+++ Build/pocha/src/main/java/com/project/pocha/controller/MainController.java
@@ -4,15 +4,13 @@ import com.project.pocha.domain.user.User;
 import com.project.pocha.domain.user.UserRepository;
 import com.project.pocha.model.UserResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin
 public class MainController {
 	@Autowired
 	UserRepository userRepository;
