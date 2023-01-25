fb41f639f5edfdcc8a78d4ef18fe3238d83fba6d
KHminor <ghdals2020@gmail.com>
2023-01-26 03:13:11 +0900

    Rename : build Error 수정

--- front-client/src/components/Login/Login.tsx
+++ front-client/src/components/Login/Login.tsx
@@ -1,19 +1,10 @@
-
-import backimg from "../../assets/img/background_Login.png";
-import styles from "./Login.module.css";
-
 function Login(): JSX.Element {
   return (
     //tailwind 경계 사이즈변경
     <div className="w-screen h-screen flex">
       <div className="w-full">
-
-      
         <img src={require("../../assets/img/background_Login.png")} alt="" className="w-px h-auto" />
-
-
         {/* <div className={styles.back}>asdasd</div> */}
-
         <div>방구석 포차</div>
         <div>Login해보자</div>
       </div>
