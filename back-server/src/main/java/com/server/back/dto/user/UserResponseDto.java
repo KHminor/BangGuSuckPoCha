95484893deae2683cdd41dedd24fa5c66fd5cd6f
eunjin <yej6642@naver.com>
2023-01-26 16:14:44 +0900

    Feat : 추가 userInfo, nicknameCheck 완료

--- back-server/src/main/java/com/server/back/dto/user/UserResponseDto.java
+++ back-server/src/main/java/com/server/back/dto/user/UserResponseDto.java
@@ -18,7 +18,7 @@ public class UserResponseDto {
     private String nickname;
     private String gender;
     private String birth;
-    private Region region;
+    private String region;
     private String profile;
     private Double manner;
     private String comment;
@@ -29,12 +29,11 @@ public class UserResponseDto {
         this.nickname = e.getNickname();
         this.gender = e.getGender();
         this.birth = e.getBirth();
-//        String sidoName = e.getRegion().getSidoName();
-//        String gugunName = e.getRegion().getGugunName();
-//        this.region = (sidoName + " " + (gugunName != null ? gugunName : "")).trim();
-        this.region = e.getRegion();
+        String sidoName = e.getRegion().getSidoName();
+        String gugunName = e.getRegion().getGugunName();
+        this.region = (sidoName + " " + (gugunName != null ? gugunName : "")).trim();
         this.profile = e.getProfile();
-        this.manner = e.getManner().doubleValue();
+        this.manner = e.getManner();
         this.comment = e.getComment();
         this.point = e.getPoint();
     }
