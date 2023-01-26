8ecdd56e5aabec46da845b83f385731f3066ec75
susu4788 <susu4788@naver.com>
2023-01-26 15:32:27 +0900

    Fix : 수정 @pathvariable, @requestparam 오류

--- back-server/src/main/java/com/server/back/controller/GameController.java
+++ back-server/src/main/java/com/server/back/controller/GameController.java
@@ -9,10 +9,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.websocket.server.PathParam;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -47,7 +47,7 @@ public class GameController {
     }
     @ApiOperation(value = "밸런스 게임 데이터")
     @GetMapping("/ysc/{type}")
-    public ResponseEntity<?> gameBalance(@PathParam(value = "type") Long type){
+    public ResponseEntity<?> gameBalance(@PathVariable(value = "type") Long type){
         Map<String,Object> response = new HashMap<>();
 
         List<BalanceResponseDto> responseDtoList = gameService.gameBalance(type);
