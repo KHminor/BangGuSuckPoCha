d6f2fb51c5fd2f95238480b095d1c3805a03ce22
susu4788 <susu4788@naver.com>
2023-01-31 21:17:44 +0900

    Feat : 추가 cors 설정

--- back-server/src/main/java/com/server/back/config/WebConfig.java
+++ back-server/src/main/java/com/server/back/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowCredentials(true)
+                //.allowCredentials(true)
                 .allowedMethods("*")
                 .maxAge(3600);
     }
