95ee681b9e7d567531c8b5cf96cfa624db54d830
eunjin <yej6642@naver.com>
2023-02-01 09:15:16 +0900

    Test : 변경 https://localhost:3000

--- back-server/src/main/java/com/server/back/service/user/NaverService.java
+++ back-server/src/main/java/com/server/back/service/user/NaverService.java
@@ -28,7 +28,7 @@ public class NaverService {
 
     private final String client_id = "ZQnQO8XghTL7eTyln27j";
     private final String client_secret = "E_N2HQiJc4";
-    private final String redirect_uri = "http://i8e201.p.ssafy.io/api/login/oauth2/code/naver";
+    private final String redirect_uri = "http://localhost:3000/api/login/oauth2/code/naver";
 //    private final String client_id = "Pi2zJMcupNEz5EsZRzh6";
 //    private final String client_secret = "ZGtXcgsvcR";
 //    private final String redirect_uri = "http://localhost:9999/api/login/oauth2/code/naver";
