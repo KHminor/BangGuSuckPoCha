53c38ea7157e234767de54488bce9bc40769b240
eunjin <yej6642@naver.com>
2023-02-02 08:58:10 +0900

    Feat : 수정 https://i8e201.p.ssafy.io

--- back-server/src/main/java/com/server/back/controller/UserController.java
+++ back-server/src/main/java/com/server/back/controller/UserController.java
@@ -47,18 +47,18 @@ public class UserController {
         TokenDto oauthToken = naverService.getAccessToken(code);
         User saveUser = naverService.saveUser(oauthToken.getAccess_token());
         if (saveUser.getRole().equals("SECESSION")){
-            String target = "http://localhost:3000/loginloading?Role=SECESSION";
+            String target = "https://i8e201.p.ssafy.io/loginloading?Role=SECESSION";
             RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
             redirectStrategy.sendRedirect(request, response, target);
         } else if (saveUser.getRole().equals("NEWBIE")){
             TokenRequestDto tokenRequestDto = jwtService.joinJwtToken(saveUser.getUsername());
-            String target = "http://localhost:3000/loginloading?Auth=" + tokenRequestDto.getAccessToken() + "&Refresh=" + tokenRequestDto.getRefreshToken() + "&Role=" + saveUser.getRole();
+            String target = "https://i8e201.p.ssafy.io/loginloading?Auth=" + tokenRequestDto.getAccessToken() + "&Refresh=" + tokenRequestDto.getRefreshToken() + "&Role=" + saveUser.getRole();
             userService.roleChange(saveUser.getUsername());
             RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
             redirectStrategy.sendRedirect(request, response, target);
         } else{
             TokenRequestDto tokenRequestDto = jwtService.joinJwtToken(saveUser.getUsername());
-            String target = "http://localhost:3000/loginloading?Auth=" + tokenRequestDto.getAccessToken() + "&Refresh=" + tokenRequestDto.getRefreshToken() + "&Role=" + saveUser.getRole();
+            String target = "https://i8e201.p.ssafy.io/loginloading?Auth=" + tokenRequestDto.getAccessToken() + "&Refresh=" + tokenRequestDto.getRefreshToken() + "&Role=" + saveUser.getRole();
             RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
             redirectStrategy.sendRedirect(request, response, target);
         }
