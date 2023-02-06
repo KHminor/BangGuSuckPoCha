8c1fdd06dc95268f63227b6579c7e20ba9f2a136
susu4788 <susu4788@naver.com>
2023-02-06 11:15:03 +0900

    Feat : 추가 소통 포차 기능

--- rtc-server/src/server.js
+++ rtc-server/src/server.js
@@ -32,39 +32,45 @@ let socketToRoom = {};
 const maximum = 6;
 
 wsServer.on("connection", (socket) => {
-  socket.on("join_room", ({ roomName, username }) => {
+  socket.on("join_room", ({ roomName, username, nickname }) => {
     if (users[roomName]) {
       const length = users[roomName].length;
       if (length == maximum) {
         socket.emit("room_full");
         return;
       }
-      users[roomName].push({ id: socket.id, user: username });
+      socket.emit("users_of_room", users[roomName]);
+      users[roomName].push({
+        id: socket.id,
+        username: username,
+        nickname: nickname,
+      });
     } else {
-      users[roomName] = [{ id: socket.id, user: username }];
+      users[roomName] = [
+        { id: socket.id, username: username, nickname: nickname },
+      ];
     }
     socketToRoom[socket.id] = roomName;
 
     socket.join(roomName);
 
-    // 유저 확인용
-    socket.to(roomName).emit("welcome", socket.id, users[roomName]);
+    socket.to(roomName).emit("welcome", socket.id, { username, nickname });
   });
-  socket.on("offer", (offer, socketId, roomName) => {
-    socket.to(socketId).emit("offer", offer, socket.id);
+  socket.on("offer", (offer, socketId, roomName, userInfo) => {
+    socket.to(socketId).emit("offer", offer, socket.id, userInfo);
   });
   socket.on("answer", (answer, socketId, roomName) => {
     socket.to(socketId).emit("answer", answer, socket.id);
   });
-  socket.on("ice", (ice, socketId, roomName) => {
-    socket.to(socketId).emit("ice", ice, socket.id);
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice, socket.id);
   });
 
   socket.on("disconnect", () => {
     const roomID = socketToRoom[socket.id];
     delete socketToRoom[socket.id];
     let room = users[roomID];
-    socket.leave(roomID)
+    socket.leave(roomID);
     if (room) {
       room = room.filter((user) => user.id !== socket.id);
       users[roomID] = room;
@@ -75,4 +81,28 @@ wsServer.on("connection", (socket) => {
     }
     socket.to(roomID).emit("user_exit", { id: socket.id });
   });
+
+  /////////////////////////////////////////////////
+  // 포차 기능!!
+
+  // 썰 변경
+  socket.on("ssul_change", (roomName, ssul) => {
+    wsServer.to(roomName).emit("ssul_change", ssul);
+  });
+
+  // 포차 설정 변경
+  socket.on("pocha_change", (roomName) => {
+    wsServer.to(roomName).emit("pocha_change");
+  });
+
+  // 포차 시간 연장
+  socket.on("pocha_extension", (roomName) => {
+    wsServer.to(roomName).emit("pocha_extension");
+  });
+
+  // 포차 짠 기능.
+  socket.on("pocha_cheers", (roomName) => {
+    wsServer.to(roomName).emit("pocha_cheers");
+  });
+  /////////////////////////////////////////////////
 });
