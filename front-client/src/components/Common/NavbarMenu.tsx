fc477c7e0ed70dfcd3b966456be8db3713c0c2af
KHminor <ghdals2020@gmail.com>
2023-02-11 16:56:57 +0900

    Feat : 메뉴,알람,친구,채팅 바탕화면 클릭시 꺼지도록 하기 해결

--- front-client/src/components/Common/NavbarMenu.tsx
+++ front-client/src/components/Common/NavbarMenu.tsx
@@ -113,7 +113,9 @@ function NavbarMenu(): JSX.Element {
               })
               console.log('베프: ',bestFriend)
               console.log('친구: ',normalFriend)
-              
+              if (checkMenuState) {
+                dispatch(changeMenuState());
+              }
               dispatch(changeMenuFriendState());
               dispatch(changeMenuFriendListApiDataState([...bestFriend,...normalFriend]));
             });
