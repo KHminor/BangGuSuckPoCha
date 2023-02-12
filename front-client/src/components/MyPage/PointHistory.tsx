320e0db21c52ffd4c967b6be16380dab25828c24
wndjf11 <wndjf11@naver.com>
2023-02-12 23:06:54 +0900

    Feat : pointlist 완성

--- front-client/src/components/MyPage/PointHistory.tsx
+++ front-client/src/components/MyPage/PointHistory.tsx
@@ -6,10 +6,12 @@ import FriendList from "../Common/FriendList";
 import Navbar from "../Common/Navbar";
 import NavbarAlarm from "../Common/NavbarAlarm";
 import NavbarMenu from "../Common/NavbarMenu";
+import styles from "../Common/Common.module.css";
 
 const PointHistory = () => {
   const Username = localStorage.getItem("Username");
-  const [History, setHistory] = useState();
+  const [History, setHistory] = useState<any>();
+  const [MyInfo, setMyInfo] = useState<any>();
   //  메뉴 -> 친구 클릭 -> 채팅 상태
   const menuFriendChatClickCheck: any = useAppSelector((state: any) => {
     return state.menuFriendChatClickCheck;
@@ -22,6 +24,14 @@ const PointHistory = () => {
     }).then((r) => {
       console.log(r.data.data);
       setHistory(r.data.data);
+    });
+
+    axios({
+      method: "get",
+      url: `https://i8e201.p.ssafy.io/api/user/myinfo/${Username}`,
+    }).then((r) => {
+      console.log(r.data.data);
+      setMyInfo(r.data.data);
     });
   }, []);
 
@@ -51,14 +61,14 @@ const PointHistory = () => {
                       포인트
                     </div>
                     <div className="h-[50%] text-justify text-[20px]">
-                      (이름)님 환영합니다.
+                      {MyInfo ? <>{MyInfo.nickname}님 환영합니다.</> : null}
                     </div>
                   </div>
                   <div className="h-[2rem] text-2xl ">현재 포인트</div>
                   <div className="h-[4rem] text-5xl font-extrabold text-sky-500 ">
-                    10000p
+                    {MyInfo ? <>{MyInfo.point}p</> : null}
                   </div>
-                  <div className="h-[4rem] w-[13rem] text-base font-extrabold border-2">
+                  <div className="h-[4rem] w-[13rem] text-justify pt-4 text-[25px] font-extrabold">
                     포인트 내역
                   </div>
                 </div>
@@ -66,7 +76,53 @@ const PointHistory = () => {
               </div>
               <div className="flex flex-row h-[55%] ">
                 <div className="w-[30%] "></div>
-                <div className="w-[40%] ">가운데</div>
+                <div
+                  className={`flex flex-col-reverse  w-[40%] pt-3 max-h-[25rem] ${styles.hideScroll}`}
+                  style={{ overflow: "auto" }}
+                >
+                  {History ? (
+                    <>
+                      {History.map((it: any): any => {
+                        return (
+                          <div className="flex justify-center w-full h-[8rem] pt-2 ">
+                            <div className="flex flex-row justify-between w-[90%] h-[100px] bg-white rounded-2xl border-2">
+                              <div className="flex flex-col pt-5 w-[30%] text-center h-full">
+                                <div className="font-bold text-[20px] text-black">
+                                  {it.content ===
+                                  "Welcome to BangGuSuck Pocha!" ? (
+                                    <>출석체크</>
+                                  ) : it.content === "썰변경" ? (
+                                    <>썰변경</>
+                                  ) : null}
+                                </div>
+                                <div className="text-neutral-400 font-bold">
+                                  {it.create_at.split("T")[0].split("-")[0]}.
+                                  {it.create_at.split("T")[0].split("-")[1]}.
+                                  {it.create_at.split("T")[0].split("-")[2]}
+                                </div>
+                              </div>
+                              <div className="flex flex-col pt-3 w-[40%] text-center h-full">
+                                {Number(it.amount) > 0 ? (
+                                  <div className="font-extrabold text-[25px] text-blue-800">
+                                    {it.amount}P
+                                  </div>
+                                ) : (
+                                  <div className="font-extrabold text-[25px] text-red-700">
+                                    {it.amount}P
+                                  </div>
+                                )}
+
+                                <div className="text-neutral-400 font-bold">
+                                  {it.current_point}P
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </>
+                  ) : null}
+                </div>
                 <div className="w-[30%] "></div>
               </div>
             </div>
