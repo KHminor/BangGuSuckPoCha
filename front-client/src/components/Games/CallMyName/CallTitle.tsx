0f682660ec87f0dbf38df7d88fc07ff721366bcd
eunjin <yej6642@naver.com>
2023-02-14 11:52:52 +0900

    Feat : 추가 call
    Former-commit-id: 3ba6ab46fafffa62caf4d630181890ce58870336

--- front-client/src/components/Games/CallMyName/CallTitle.tsx
+++ front-client/src/components/Games/CallMyName/CallTitle.tsx
@@ -1,24 +1,57 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import styles from "./CallTitle.module.css";
 
 function CallTitle({
   socket,
   pochaId,
   pochaUsers,
+  pochaInfo,
 }: {
   socket: any;
   pochaId: string;
   pochaUsers: any;
+  pochaInfo: any;
 }): React.ReactElement {
   const roomName = pochaId;
 
+  const [titles, setTitles] = useState<any>(null)
+
+  const [nowtitle, setNowtitle] = useState<any>(null);
+
+  // 양세찬 게임 주제 받아오기
+  const getCallSubject = async() => {
+    try {
+      const {
+        data: { data },
+      } = await axios({
+        url: `https://i8e201.p.ssafy.io/api/pocha/game/ysc`,
+      });
+      setTitles(data);
+    } catch (error) {
+      console.log("라이어 게임 주제 axios error", error);
+    }
+  }
+
+  const titlechoice = () => {
+    for (var i = 0; i < pochaInfo.totalCount ; i++) {
+      var newnum = titles.splice(Math.floor(Math.random() * titles.length),1)[0]
+      setNowtitle(newnum)
+      console.log(nowtitle);
+    }
+  }
+
+  useEffect(()=>{
+    getCallSubject();
+  },[])
+
+  useEffect(()=>{
+    titlechoice();
+  },[titles])
+
+
   const onClickClose = () => {
     const signalData = "INPUT";
-    // 선택창으로 돌아가기
-    socket.emit("game_call_signal", roomName, signalData);
-  };
-  
-  const onClickRetry = () => {
-    const signalData = "INTRO";
     // 선택창으로 돌아가기
     socket.emit("game_call_signal", roomName, signalData);
   };
