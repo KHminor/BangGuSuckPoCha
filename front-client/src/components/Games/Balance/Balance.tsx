10c8c8c2158246bbe217c2c11bd12c9d06c3e6de
wndjf11 <wndjf11@naver.com>
2023-02-15 09:54:10 +0900

    Feat : balance게임 token 갱신 처리
    Former-commit-id: 7b6d746b477c575a8027931d966e8738762e41f8

--- front-client/src/components/Games/Balance/Balance.tsx
+++ front-client/src/components/Games/Balance/Balance.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { useAppSelector } from "src/store/hooks";
 import "./Balance.css";
 // import styles from "./GameSelect.module.css";
@@ -17,6 +19,7 @@ function Balance({
   console.log("포차이름: ", pochaId);
 
   console.log("포차유저 정보: ", pochaUsers);
+  const navigate = useNavigate();
 
   const imgDiv = useRef<any>();
   // 플레이어 자동재생 막기
@@ -87,19 +90,114 @@ function Balance({
 
   useEffect(() => {
     // Normal
+    // axios({
+    //   method: "get",
+    //   url: "https://i8e201.p.ssafy.io/api/pocha/game/balance/0",
+    // }).then((r) => {
+    //   setGeneralDataList(r.data.data);
+    // });
+    let accessToken = localStorage.getItem("accessToken");
     axios({
       method: "get",
       url: "https://i8e201.p.ssafy.io/api/pocha/game/balance/0",
+      headers: {
+        accessToken: accessToken,
+      },
     }).then((r) => {
-      setGeneralDataList(r.data.data);
+      //토큰이상해
+      if ("401" === r.data.status) {
+        //토큰 재요청
+        console.log("토큰 이상함");
+        const refreshToken = localStorage.getItem("refreshToken");
+        const Username = localStorage.getItem("Username");
+        axios({
+          method: "get",
+          url: `https://i8e201.p.ssafy.io/api/user/auth/refresh/${Username}`,
+          headers: {
+            refreshToken: refreshToken,
+          },
+        }).then((r) => {
+          //재발급 실패
+          if ("401" === r.data.status) {
+            localStorage.clear();
+            toast.error("인증되지 않은 유저입니다");
+            navigate("/");
+          }
+          //재발급 성공
+          else {
+            console.log("재발급 성공", r.data.accessToken);
+            localStorage.setItem("accessToken", r.data.accessToken);
+            accessToken = r.data.accessToken;
+            //원래 axios 실행
+            axios({
+              method: "get",
+              url: "https://i8e201.p.ssafy.io/api/pocha/game/balance/0",
+              headers: {
+                accessToken: accessToken,
+              },
+            }).then((r) => {
+              setGeneralDataList(r.data.data);
+            });
+          }
+        });
+      }
+      //토큰 정상이야
+      else {
+        //실행 결과값 그대로 실행
+        setGeneralDataList(r.data.data);
+      }
     });
 
     // Romantic
     axios({
       method: "get",
       url: "https://i8e201.p.ssafy.io/api/pocha/game/balance/1",
+      headers: {
+        accessToken: accessToken,
+      },
     }).then((r) => {
-      setRomanticDataList(r.data.data);
+      //토큰이상해
+      if ("401" === r.data.status) {
+        //토큰 재요청
+        console.log("토큰 이상함");
+        const refreshToken = localStorage.getItem("refreshToken");
+        const Username = localStorage.getItem("Username");
+        axios({
+          method: "get",
+          url: `https://i8e201.p.ssafy.io/api/user/auth/refresh/${Username}`,
+          headers: {
+            refreshToken: refreshToken,
+          },
+        }).then((r) => {
+          //재발급 실패
+          if ("401" === r.data.status) {
+            localStorage.clear();
+            toast.error("인증되지 않은 유저입니다");
+            navigate("/");
+          }
+          //재발급 성공
+          else {
+            console.log("재발급 성공", r.data.accessToken);
+            localStorage.setItem("accessToken", r.data.accessToken);
+            accessToken = r.data.accessToken;
+            //원래 axios 실행
+            axios({
+              method: "get",
+              url: "https://i8e201.p.ssafy.io/api/pocha/game/balance/1",
+              headers: {
+                accessToken: accessToken,
+              },
+            }).then((r) => {
+              setRomanticDataList(r.data.data);
+            });
+          }
+        });
+      }
+      //토큰 정상이야
+      else {
+        //실행 결과값 그대로 실행
+        setRomanticDataList(r.data.data);
+      }
     });
   }, []);
 
