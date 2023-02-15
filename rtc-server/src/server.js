ba1920b4d0832d03de384044042b29c54d4a0d86
eunjin <yej6642@naver.com>
2023-02-16 08:43:21 +0900

    Feat : 수정 비상
    Former-commit-id: 6087a929a75c9b31617daf6169df321b4b7f010c

--- rtc-server/src/server.js
+++ rtc-server/src/server.js
@@ -298,19 +298,22 @@ wsServer.on("connection", (socket) => {
   socket.on("game_balance_Intro", (roomName, isBalance) => {
     wsServer.to(roomName).emit("game_balance_Intro", isBalance);
   });
+
   // 밸런스 게임 romantic,normal 클릭
   socket.on("game_balance_typeChange", (roomName, choiceType) => {
     console.log("choiceType?", choiceType);
     wsServer.to(roomName).emit("game_balance_typeChange", choiceType);
   });
+  
   // 밸런스 게임 테마에 따른 내용 변경
   socket.on("game_balance_subjectChange", (roomName, themeDataList) => {
     console.log("choiceType?", themeDataList);
     wsServer.to(roomName).emit("game_balance_subjectChange", themeDataList);
+  });
+
   //정한 라이어 보내기
-  socket.on("game_liar_number", (roomName, data) => {
+    socket.on("game_liar_number", (roomName, data) => {
     wsServer.to(roomName).emit("game_liar_number", data);
-    })
   });
 
   // 양세찬 게임
