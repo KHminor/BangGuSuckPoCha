c5d7160c263367b099c9bb73a31c1559b1846a7b
KHminor <ghdals2020@gmail.com>
2023-02-17 01:37:07 +0900

    Test : 수정 채팅 JWT 내림
    Former-commit-id: 77e3d4d9d4f0a39c39e6348a3151138adb9b7184

--- front-client/src/components/Common/FriendChat.tsx
+++ front-client/src/components/Common/FriendChat.tsx
@@ -98,18 +98,16 @@ function FriendChat():JSX.Element {
     
     client.current = new StompJs.Client({
       brokerURL: 'wss://i8e201.p.ssafy.io/api/ws/chat', // 왜 websocket을 붙여줘야하는거지..?
-      connectHeaders: {
-        "Content-Type": "text/event-stream;charset=utf-8",
-        accessToken: `${accessToken}`,
-        // "Authorization": `Bearer ${accessToken}`,
-      },
+      // connectHeaders: {
+      //   "Content-Type": "text/event-stream;charset=utf-8",
+      //   accessToken: `${accessToken}`,
+      //   // "Authorization": `Bearer ${accessToken}`,
+      // },
       webSocketFactory: () => new SockJS("https://i8e201.p.ssafy.io/api/ws/chat"),
       debug: function (str) {
           console.log(str);
       },
-      reconnectDelay: 5000,
-      heartbeatIncoming: 4000,
-      heartbeatOutgoing: 4000,
+
       onConnect:(frame) => { 
         console.log(frame);
         
